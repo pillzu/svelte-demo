bc69704f470207fc4824781cdc10bfcbcae520e0
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-08-17 12:03:35 -0400

    api-endpoints-complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+	async function subscribe(event: Event) {
+		const form = event.target as HTMLFormElement
+		const data = new FormData(form)
+
+		await fetch('api/newsletter', {
+			method: 'POST',
+			body: data
+		})
+	}
 </script>
 
-<h1>Hello</h1>
+<h1>Newsletter</h1>
+
+<form on:submit|preventDefault={subscribe}>
+	<input type="email" name="email" />
+	<button>Subscribe</button>
+</form>
