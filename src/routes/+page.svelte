fa15366225c1de6bce27849922ffdff02caadabc
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-08-24 11:23:18 -0400

    resuse login form action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,31 @@
-<h1>Form</h1>
+<script lang="ts">
+	import { applyAction, enhance } from '$app/forms'
+	import type { SubmitFunction } from '@sveltejs/kit'
+	import type { ActionData } from './$types'
 
-<form method="POST" action="/login">
-	<input type="text" name="user" />
+	export let form: ActionData
+
+	const login: SubmitFunction = () => {
+		return async ({ result }) => {
+			await applyAction(result)
+		}
+	}
+</script>
+
+<form method="POST" action="/login" use:enhance={login}>
+	<input type="text" name="user" value={form?.data?.user ?? ''} />
+	{#if form?.errors?.user}
+		<p class="error">Name is required</p>
+	{/if}
 	<input type="password" name="password" />
+	{#if form?.errors?.password}
+		<p class="error">Password is required</p>
+	{/if}
 	<button type="submit">Login</button>
 </form>
+
+<style>
+	.error {
+		color: tomato;
+	}
+</style>
